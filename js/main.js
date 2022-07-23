13adfc81ebdd0e1591c3339d7d66fdc0e7fa8834
punkish <punk.kish@gmail.com>
2022-07-23 14:25:49 +0200

    added map

--- js/main.js
+++ js/main.js
@@ -49,6 +49,12 @@ const formatRecords = (resource, records) => {
 
 pager = (_links) => `<a href="${_links._prev}" class="page">prev</a> <a href="${_links._next}" class="page">next</a>`;
 
+const resources = {
+    treatments: 'Taxon Treatments',
+    images: 'Images',
+    publications: 'Publications'
+}
+
 const getResults = async (resource, url) => {
     const response = await fetch(`${z}/${resource}?${url}`);
 
@@ -62,8 +68,7 @@ const getResults = async (resource, url) => {
         const facets = json.item.result.facets;
         const _links = json.item._links;
 
-        let innerHTML = $(`#${resource}`).innerHTML;
-        $(`#${resource}`).innerHTML = `${innerHTML} ${count}`;
+        $(`#${resource}`).innerHTML = `${resources[resource]} ${count}`;
 
         innerHTML = formatRecords(resource, records);
         $('#results').innerHTML = innerHTML;
@@ -127,3 +132,80 @@ const pages = {
 
 $$('form button').forEach(b => b.addEventListener('click', go));
 $('form').addEventListener('submit', go);
+
+
+const search = function(e) {
+    const search_results = document.getElementById('search_results')
+
+    fetch('https://tb.plazi.org/GgServer/srsStats/stats?outputFields=doc.type+doc.uuid+doc.name+doc.uploadDate+bib.author+bib.title+tax.name&groupingFields=doc.type+doc.uuid+doc.name+doc.uploadDate+bib.author+bib.title+tax.name&FP-doc.uploadDate=%222021-07-16%22&format=JSON')
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('HTTP error, status = ' + response.status)
+            }
+
+            return response.json()
+        })
+        .then(function(response) {
+            const data = response.data
+            let res = '<h2>Latest Treatments</h2>'
+            data.forEach(r => {
+                res += '<details><summary>' + r.TaxName + '</summary><p>' + r.BibTitle + ', ' + r.BibAuthor + '</p></details>'
+            })
+
+            search_results.innerHTML = res
+        })
+
+    e.stopPropagation()
+    e.preventDefault()
+}
+
+    // window.onload = function(e) {
+    //     const searchBtn = document.getElementById('searchBtn')
+    //     searchBtn.addEventListener('click', search)
+    // }()
+const loadMap = () => {
+    fetch('https://tb.plazi.org/GgServer/srsStaticStats/latestMapPoints.json', {mode: 'cors'})
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('HTTP error, status = ' + response.status);
+            }
+
+            return response.json();
+        })
+        .then(function(response) {
+        const points = response.data
+        const center = [0, 0]
+        //const accessToken = 'pk.eyJ1IjoiZmljaHQiLCJhIjoiY2locWQ3YTBtMDAxYnY1bHVvcGtsM2Y1MCJ9.C8NlGmZuX6W2YrvXTHULeQ'
+        const mymap = L.map('map').setView(center, 2)
+
+        // initialize the baselayer
+        //const mapurl = 'https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
+        const mapurl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        L.tileLayer(mapurl, {
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(mymap);
+
+        const url = 'https://tb.plazi.org/GgServer/html/';
+
+        const icon = L.icon({ 
+            iconUrl: '/img/treatment.png', 
+            iconSize: [10, 10],
+            iconAnchor: [0, 0],
+            popupAnchor: [6, 5]
+        });
+
+        for (let i = 0, j = points.length; i < j; i++) {
+            const p = points[i]
+            const popupHtml =  
+                `<div class='TaxName'>
+                    <a href='${url}/${p.DocUuid}' target='_blank'>${p.TaxName}</a>
+                </div>
+                <div class='BibDspTitle'>${p.BibDspTitleDsp}</div>
+                <div class='BibOrigin'>${p.BibOrigin}</div>`
+            L.marker([Number(p.MatCitLatitude), Number(p.MatCitLongitude), ], { icon: icon })
+                .bindPopup(popupHtml)
+                .addTo(mymap)
+        }
+    })
+}
